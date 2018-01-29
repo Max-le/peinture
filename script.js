90b871e185c323a51d5c061fa93c26c410356ccf
Max-le <max.09@outlook.com>
2018-01-29 19:07:12 +0100

    more fade in

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log('Hello world !')
 
+
 $(document).ready(function() {
   $( "#hello" ).fadeIn(2000);
 
   $("#description").fadeIn(5000);
+  $(".row").fadeIn(1500);
+  $(".galerie").fadeIn(2500);
+
 });
