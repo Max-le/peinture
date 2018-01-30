bc663b3a6e59d0ac6b35622b3be0ab6cb4c58105
Max-le <max.09@outlook.com>
2018-01-30 14:56:50 +0100

    remove unnesseray lines

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
   $( "#hello" ).fadeIn(2000);
 
   $("#description").fadeIn(5000);
-  // $(".row").fadeIn(1500);
-  // $(".fond").fadeIn(1500);
+
   $(".jumbotron").delay(1000).fadeIn(2000);
 
   $(".galerie").fadeIn(2500);
