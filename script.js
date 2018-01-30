bde457bdb98242f503c0c26bb50bdd291009e178
Max-le <max.09@outlook.com>
2018-01-30 17:05:08 +0100

    solve override jumbotron no mobile

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ console.log('Hello world !')
 
 
 $(document).ready(function() {
-  $( "#hello" ).fadeIn(2000);
 
-  $("#description").fadeIn(5000);
-
-  $(".jumbotron").delay(1000).fadeIn(2000);
 
   $(".galerie").fadeIn(2500);
 
+});
+$(".jumbotron").ready(function() {
+console.log("Jumbotron ready !");
+
+$( "#hello" ).fadeIn(2000);
+
+$("#description").fadeIn(5000);
+
+$(".jumbotron").delay(1000).fadeIn(2000);
 });
