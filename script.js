9a721591a90cdf35db653017f5d138622d21ca73
Max-le <max.09@outlook.com>
2018-01-30 14:51:49 +0100

    remove unnesseray lines

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ $(document).ready(function() {
   $( "#hello" ).fadeIn(2000);
 
   $("#description").fadeIn(5000);
-  $(".row").fadeIn(1500);
-  $(".fond").fadeIn(1500);
-  $(".jumbotron").delay(1000).fadeIn(2000); 
-
+  // $(".row").fadeIn(1500);
+  // $(".fond").fadeIn(1500);
+  $(".jumbotron").delay(1000).fadeIn(2000);
 
   $(".galerie").fadeIn(2500);
 
